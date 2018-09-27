17b4ada764b5ede63135f7d0a32537714fcbb9ef
gmanipon <gmanipon@jpl.nasa.gov>
2018-09-27 15:04:17 +0000

    create CUDA-based HySDS base image

--- build_docker.sh
+++ build_docker.sh
@@ -7,4 +7,12 @@ if [ "$#" -ne 1 ]; then
 fi
 TAG=$1
 
+# pull latest version of base images
+docker pull docker.io/centos:7 || exit 1
+docker tag docker.io/centos:7 docker.io/centos:latest || exit 1
+docker pull nvidia/cuda:9.2-runtime-centos7 || exit 1
+docker tag nvidia/cuda:9.2-runtime-centos7 nvidia/cuda:latest || exit 1
+
+# build base images
 docker build --rm --force-rm -t hysds/base:${TAG} -f docker/Dockerfile .
+docker build --rm --force-rm -t hysds/cuda-base:${TAG} -f docker/Dockerfile.cuda .

a1f3c9b4347e6a9d170399edcc81b47a64223d80
Gerald Manipon <pymonger@gmail.com>
2019-02-14 15:55:46 +0000

    install future using conda

--- manifests/init.pp
+++ manifests/init.pp
@@ -205,7 +205,7 @@ class hysds_base {
   anaconda { 'packages':
     path    => $conda_path,
     action  => 'install',
-    args    => '-y virtualenv libxml2 libxslt cython cartopy',
+    args    => '-y virtualenv libxml2 libxslt cython cartopy future',
     require => Anaconda['update_all'],
   }
 

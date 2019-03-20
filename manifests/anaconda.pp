1b7201d26f007f7e2586c59d6eea1c2728558ff0
Gerald Manipon <pymonger@gmail.com>
2019-03-20 09:30:30 -0700

    python3 port (#3)
    * port to python3 (#1)
    * install python3 via miniconda
    * clone specified branch
    * call with branch arg
    * install updated dependencies: dbxml and bsddb3
    * install dbxml/bsddb3 after anaconda installation
    * install libxml2/libxslt via conda; remove custom eggs for pyxml and processing
    * install m2crypto and soappy
    * remove dependency on m2crypto and soappy
    * remove SOAPpy egg
    * update documentation
    * install cython and cartopy via conda (#2)
    * install python3 via miniconda
    * clone specified branch
    * call with branch arg
    * install updated dependencies: dbxml and bsddb3
    * install dbxml/bsddb3 after anaconda installation
    * install libxml2/libxslt via conda; remove custom eggs for pyxml and processing
    * install m2crypto and soappy
    * remove dependency on m2crypto and soappy
    * remove SOAPpy egg
    * update documentation
    * install cython and cartopy via conda
    * install future using conda
    * remove installation of firewalld; leave to downstream puppet modules
    * add example for python2

---
+++ manifests/anaconda.pp
@@ -0,0 +1,61 @@
+define hysds_base::anaconda($path='/opt/conda', $action=install_miniconda, $args='') {
+  case $action {
+    install_miniconda: {
+      exec { "install_anaconda":
+        path    => "/usr/local/bin:/usr/bin:/bin",
+        command => "/tmp/miniconda.sh -b -p $path",
+        creates => $path,
+        require => File["/tmp/miniconda.sh"],
+        notify  => Exec["remove_installer"],
+      }
+
+      exec { "download_installer":
+        path    => "/usr/local/bin:/usr/bin:/bin",
+        command => "curl -sSL https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -o /tmp/miniconda.sh",
+        creates => "/tmp/miniconda.sh", 
+      }
+
+      file { "/tmp/miniconda.sh":
+        ensure => present,
+        mode   => 0755,
+        require => Exec["download_installer"],
+      }
+
+      exec { "remove_installer":
+        path    => "/usr/local/bin:/usr/bin:/bin",
+        command => "rm -rf /tmp/miniconda.sh",
+      }
+    }
+
+    pin: {
+      exec { "touch ${path}/conda-meta/pinned":
+        path    => "/usr/local/bin:/usr/bin:/bin",
+        creates => inline_template("${path}/conda-meta/pinned"),
+      }
+    }
+
+    config: {
+      exec { "conda config $args":
+        path    => "${path}/bin:/usr/local/bin:/usr/bin:/bin",
+      }
+    }
+
+    update: {
+      exec { "conda update $args":
+        path    => "${path}/bin:/usr/local/bin:/usr/bin:/bin",
+      }
+    }
+
+    install: {
+      exec { "conda install $args":
+        path    => "${path}/bin:/usr/local/bin:/usr/bin:/bin",
+      }
+    }
+
+    clean: {
+      exec { "conda clean --all":
+        path    => "${path}/bin:/usr/local/bin:/usr/bin:/bin",
+      }
+    }
+  }
+}
--- manifests/easy_install.pp
+++ manifests/easy_install.pp
@@ -1,15 +1,15 @@
 define hysds_base::easy_install($ensure = installed) {
   case $ensure {
     installed: {
-      exec { "/usr/bin/easy_install $name":
-        path    => "/usr/local/bin:/usr/bin:/bin",
-        creates => inline_template("/usr/lib/python2.7/site-packages/<%= File.basename(@name) %>"),
+      exec { "/opt/conda/bin/easy_install $name":
+        path    => "/opt/conda/bin:/usr/local/bin:/usr/bin:/bin",
+        creates => inline_template("/usr/lib/python3.7/site-packages/<%= File.basename(@name) %>"),
         timeout => 1800,
       }
     }
     latest: {
-      exec { "/usr/bin/easy_install --upgrade $name":
-        path    => "/usr/local/bin:/usr/bin:/bin",
+      exec { "/opt/conda/bin/easy_install --upgrade $name":
+        path    => "/opt/conda/bin:/usr/local/bin:/usr/bin:/bin",
         timeout => 1800,
       }
     }

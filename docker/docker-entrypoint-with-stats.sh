2eabd15abb809a66bbde909172a6714c2760d748
Gerald Manipon <pymonger@gmail.com>
2019-11-26 07:59:52 -0800

    copy entrypoint with stats enabled

---
+++ docker/docker-entrypoint-with-stats.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+# set HOME explicitly
+export HOME=/home/ops
+
+# get group id
+GID=$(id -g)
+
+# update user and group ids
+gosu 0:0 groupmod -g $GID ops 2>/dev/null
+gosu 0:0 usermod -u $UID -g $GID ops 2>/dev/null
+gosu 0:0 usermod -aG docker ops 2>/dev/null
+
+# update ownership
+gosu 0:0 chown -R $UID:$GID $HOME 2>/dev/null || true
+gosu 0:0 chown -R $UID:$GID /var/run/docker.sock 2>/dev/null || true
+
+# source bash profile
+source $HOME/.bash_profile
+
+exec gosu $UID:$GID /docker-stats-on-exit-shim _docker_stats.json "$@"

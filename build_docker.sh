8b265e55ed38e5e19b6cb08fe52a61703ee3f005
Albert Li <albert.li@vroozi.com>
2020-08-25 05:58:06 -0700

    Update build_docker.sh

--- build_docker.sh
+++ build_docker.sh
@@ -12,8 +12,8 @@ BRANCH=$3
 # pull latest version of base images
 docker pull hysds/centos:7 || exit 1
 docker tag hysds/centos:7 docker.io/centos:latest || exit 1
-docker pull hysds/cuda:9.2-runtime-centos7 || exit 1
-docker tag hysds/cuda:9.2-runtime-centos7 nvidia/cuda:latest || exit 1
+docker pull nvidia/cuda:9.2-runtime-centos7 || exit 1
+docker tag nvidia/cuda:9.2-runtime-centos7 nvidia/cuda:latest || exit 1
 
 # build base images
 docker build --rm --force-rm --build-arg ORG=${ORG} --build-arg BRANCH=${BRANCH} \

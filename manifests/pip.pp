e659c66b13394717caca936d262b5e44e78ed0bd
Gerald Manipon <pymonger@gmail.com>
2021-02-16 12:49:53 -0800

    update pip to install wheels; install dbxml and bsddb3 wheels via pip

--- manifests/pip.pp
+++ manifests/pip.pp
@@ -1,20 +1,26 @@
-define hysds_base::pip($ensure = installed) {
+define hysds_base::pip($ensure = installed, $wheel = "") {
+  if $wheel != "" {
+    $pkg = $wheel
+  }
+  else {
+    $pkg = $name
+  } 
   case $ensure {
     installed: {
-      exec { "/opt/conda/bin/pip install $name":
+      exec { "/opt/conda/bin/pip install $pkg":
         path => "/opt/conda/bin:/usr/local/bin:/usr/bin:/bin",
         unless => "/opt/conda/bin/pip show $name",
         timeout => 1800,
       }
     }
     latest: {
-      exec { "/opt/conda/bin/pip install --upgrade $name":
+      exec { "/opt/conda/bin/pip install --upgrade $pkg":
         path => "/opt/conda/bin:/usr/local/bin:/usr/bin:/bin",
         timeout => 1800,
       }
     }
     default: {
-      exec { "/opt/conda/bin/pip install $name==$ensure":
+      exec { "/opt/conda/bin/pip install $pkg==$ensure":
         path => "/opt/conda/bin:/usr/local/bin:/usr/bin:/bin",
         timeout => 1800,
       }

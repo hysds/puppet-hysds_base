10ce3a77acffa7560a349663f75aff6a7a2d8b06
Gerald Manipon <pymonger@gmail.com>
2022-02-09 04:52:39 +0000

    test

--- build_docker.sh
+++ build_docker.sh
@@ -11,6 +11,8 @@ BRANCH=$3
 
 # build base images
 buildah build --rm --force-rm --layers --build-arg ORG=${ORG} --build-arg BRANCH=${BRANCH} \
-  -t hysds/base:${TAG} -f docker/Dockerfile .
+  -t hysds/base:develop-podman -f docker/Dockerfile.test .
+#buildah build --rm --force-rm --layers --build-arg ORG=${ORG} --build-arg BRANCH=${BRANCH} \
+#  -t hysds/base:${TAG} -f docker/Dockerfile .
 #buildah build --rm --force-rm --layers --build-arg TAG=${TAG} --build-arg ORG=${ORG} \
 #  --build-arg BRANCH=${BRANCH} -t hysds/cuda-base:${TAG} -f docker/Dockerfile.cuda .

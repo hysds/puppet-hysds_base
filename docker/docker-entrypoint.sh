76977c98617171e463301a4158774f1fbafb8de0
Mike Cayanan <michael.d.cayanan@jpl.nasa.gov>
2024-05-02 16:24:36 -0700

    update entrypoints

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -8,13 +8,18 @@ export HOME=/home/ops
 GID=$(id -g)
 
 ## update user and group ids
-gosu 0:0 groupmod -g $GID ops 2>/dev/null
-gosu 0:0 usermod -u $UID -g $GID ops 2>/dev/null
-#gosu 0:0 usermod -aG docker ops 2>/dev/null
+#gosu 0:0 groupmod -g $GID ops 2>/dev/null
+#gosu 0:0 usermod -u $UID -g $GID ops 2>/dev/null
+if id -u "docker" >/dev/null 2>&1; then
+  gosu 0:0 usermod -aG docker ops 2>/dev/null
+fi
 
 # update ownership
 gosu 0:0 chown -R $UID:$GID $HOME 2>/dev/null || true
-#gosu 0:0 chown -R $UID:$GID /var/run/docker.sock 2>/dev/null || true
+
+if [ -f /var/run/docker.sock ]; then
+  gosu 0:0 chown -R $UID:$GID /var/run/docker.sock 2>/dev/null || true
+fi
 
 # source bash profile
 source $HOME/.bash_profile
